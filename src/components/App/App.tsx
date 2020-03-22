1b0a13a7fe504c4d391f18918e08481a589b0ba8
Andrew Keller <Keller192@gmail.com>
2020-03-22 19:35:04 -0400

    organizes components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Name from '../Name/Name';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Name />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Name/Name.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Name.css';
+
+function Name() {
+  return (
+    <div className="Name">
+      <img src={logo} className="Name-logo" alt="" />
+    </div>
+  );
+}
+
+export default Name;
